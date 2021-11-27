d5775d5bfae6f49d1cbc77100fb1e5e0311eba85
i-zokirov <izokirov@outlook.com>
2021-11-27 14:22:02 +0100

    Implemented product && rating components

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Col, Row } from 'react-bootstrap'
 
+import Product from '../components/Product'
+
 import products from '../products'
 
 
@@ -8,16 +10,16 @@ const HomeScreen = () => {
     
     
     return (
-        <React.Fragment>
-            <h1>Latest products</h1>
+        <>
+            <h1>Latest Products</h1>
             <Row>
-                {products.map((product)=>(
-                    <Col sm={12} md={6} lg={3}>
-                        <h3>{product.name}</h3>
+                {products.map((product) => (
+                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                        <Product product={product} />
                     </Col>
                 ))}
             </Row>
-        </React.Fragment>
+        </>
     )
 }
 
