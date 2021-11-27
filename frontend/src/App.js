310b0914bafd42b4953b0cf5f43b3d3ffdf13d8e
i-zokirov <izokirov@outlook.com>
2021-11-27 15:08:58 +0100

    Created product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ const App  = ()=>{
         <Container>
           
             <Route path="/" component={HomeScreen} exact />
-            <Route path="/product/:id" component={ProductScreen}/>
+            <Route path="/product/:productId" component={ProductScreen}/>
         
         </Container>
       </main>
