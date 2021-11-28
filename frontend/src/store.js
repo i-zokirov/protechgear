607fe49040ae4dcbc7e5a45832a6d585d1b3e2e9
i-zokirov <izokirov@outlook.com>
2021-11-28 17:10:04 +0100

    Integrated redux store and reducers with action creator for fetching products

---
+++ frontend/src/store.js
@@ -0,0 +1,16 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import {composeWithDevTools} from 'redux-devtools-extension';
+
+import { productListReducer } from "./reducers/productReducers";
+
+
+const middleware = [thunk]
+const reducer = combineReducers({
+    productList: productListReducer
+})
+const initialState = {}
+
+const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
+
+export default store
