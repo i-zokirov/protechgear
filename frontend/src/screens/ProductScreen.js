f101527da2e71b51b4625f14caa986d3c52e1da1
i-zokirov <izokirov@outlook.com>
2021-11-29 22:41:54 +0100

    Refactored rendering function

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -20,61 +20,6 @@ const ProductScreen = ({match}) => {
     const productDetails = useSelector(state=> state.productDetail)
     const {product, loading, error} = productDetails
 
-    const renderProductDetails = (product)=>{
-        return (
-            <Row>
-                    <Col md={6}>
-                        <Image src={product.image} alt={product.name} fluid/>
-                    </Col>
-        
-                    <Col md={3}>
-                        <ListGroup variant="flush">
-                            <ListGroup.Item>
-                                <h3>{product.name}</h3>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <Rating value={product.rating} text={`${product.numReviews} reviews`} />
-                            </ListGroup.Item>
-                            
-                            <ListGroup.Item>
-                                Price: ${product.price}
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                Description: {product.description}
-                            </ListGroup.Item>
-                        </ListGroup>
-                    </Col>
-
-                    <Col md={3}>
-                        <Card>
-                            <ListGroup variant="flush">
-                                <ListGroup.Item>
-                                    <Row>
-                                        <Col>Price: </Col>
-                                        <Col> <strong>${product.price}</strong> </Col>
-                                    </Row>
-                                </ListGroup.Item>
-
-                                <ListGroup.Item>
-                                    <Row>
-                                        <Col>Status: </Col>
-                                        <Col>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock' }</Col>
-                                    </Row>
-                                </ListGroup.Item>
-
-                                <ListGroup.Item>
-                                    <Button className="btn-block" type="button" disabled={product.countInStock < 0 ? 'disabled' : ''}>
-                                        Add To Cart
-                                    </Button>
-                                </ListGroup.Item>
-                            </ListGroup>
-                        </Card>
-                    </Col>
-                </Row>
-        )
-    }
-    
-
     return (
         <React.Fragment>
             <Link to="/" className="btn btn-light my-3"><i className="fas fa-arrow-left"></i> Go back</Link>
@@ -84,6 +29,60 @@ const ProductScreen = ({match}) => {
             : renderProductDetails(product)
             }
         </React.Fragment>
+    )
+}
+
+const renderProductDetails = (product)=>{
+    return (
+        <Row>
+                <Col md={6}>
+                    <Image src={product.image} alt={product.name} fluid/>
+                </Col>
+    
+                <Col md={3}>
+                    <ListGroup variant="flush">
+                        <ListGroup.Item>
+                            <h3>{product.name}</h3>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+                        </ListGroup.Item>
+                        
+                        <ListGroup.Item>
+                            Price: ${product.price}
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Description: {product.description}
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+
+                <Col md={3}>
+                    <Card>
+                        <ListGroup variant="flush">
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Price: </Col>
+                                    <Col> <strong>${product.price}</strong> </Col>
+                                </Row>
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Status: </Col>
+                                    <Col>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock' }</Col>
+                                </Row>
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                <Button className="btn-block" type="button" disabled={product.countInStock < 0 ? 'disabled' : ''}>
+                                    Add To Cart
+                                </Button>
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Card>
+                </Col>
+            </Row>
     )
 }
 
