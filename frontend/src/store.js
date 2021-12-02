aa3a417dc1718c5abfaa437ff5e2349c3835f649
i-zokirov <izokirov@outlook.com>
2021-12-02 14:02:39 +0100

    Created user login reducer & action on frontend and connected store with localstorage

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,20 +4,30 @@ import {composeWithDevTools} from 'redux-devtools-extension';
 
 import { productListReducer, productDatailsReducer } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers"
+import { userLoginReducer } from "./reducers/userReducers";
+
 
 const middleware = [thunk]
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetail: productDatailsReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    userLogin: userLoginReducer
 })
 
+// get cart items from local storage to add to the initial state of the redux store
 const cartItemsFromLocalStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+
+// get user information from local storage to add to the initial state of the redux store
+const userInfoFromLocalStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
 
 const initialState = {
     cart: {
         cartItems: cartItemsFromLocalStorage
+    },
+    userLogin: {
+        userInfo: userInfoFromLocalStorage
     }
 }
 
