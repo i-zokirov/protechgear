844c7a15d18e02519dae445424b12fa34d9636d9
i-zokirov <izokirov@outlook.com>
2021-12-05 15:21:09 +0100

    Created a route to update user profile on beckend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -21,24 +21,6 @@ export const authUser =  asyncHandler(async(req, res)=>{
 })
 
 
-export const getUserProfile = asyncHandler(async(req, res)=>{
-    const user = await User.findById(req.user._id)
-    
-    if(user){
-        res.json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            isAdmin: user.isAdmin,
-        })
-    } else {
-        res.status(404)
-        throw new Error('User not found')
-    }
-
-})
-
-
 export const registerUser =  asyncHandler(async(req, res)=>{
     const { email, password, name } = req.body
 
@@ -67,3 +49,47 @@ export const registerUser =  asyncHandler(async(req, res)=>{
     }
     
 })
+
+
+
+export const getUserProfile = asyncHandler(async(req, res)=>{
+    const user = await User.findById(req.user._id)
+    
+    if(user){
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+
+})
+
+export const updateUserProfile = asyncHandler( async(req, res)=>{
+    const user = await User.findById(req.user._id)
+    
+    if(user){
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+        if(req.body.password){
+            user.password = req.body.password
+        }
+        const updatedUser = await user.save()
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+            token: generateToken(updatedUser._id)
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+} )
+
+
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { authUser, getUserProfile, registerUser } from "../controllers/userController.js"
+import { authUser, getUserProfile, registerUser, updateUserProfile } from "../controllers/userController.js"
 import { protect } from "../middleware/authMiddleware.js"
 const router = express.Router()
 
@@ -10,5 +10,6 @@ router.post('/login', authUser)
 
 router.route('/profile')
     .get(protect, getUserProfile)
+    .put(protect, updateUserProfile)
 
 export default router
